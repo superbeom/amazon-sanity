37e519c3a2c81602cb8dd6b1ca1c7275622fb8eb
superbeom <it.super.beom@gmail.com>
2022-05-19 10:56:08 +0900

    Update stripe - Update cancel page

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
           };
         }),
         success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/canceled`,
+        cancel_url: `${req.headers.referer}`,
       };
 
       // Create Checkout Sessions from body params.
